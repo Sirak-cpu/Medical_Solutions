d18ebe7264dd824d1c793484c5d6c241ab5ea601
Sirak Embibel <embibelshimekit@gmail.com>
2025-11-14 12:19:24 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -25,6 +25,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </CartProvider>
   </React.StrictMode>
 );
-<BrowserRouter basename="/Medical_Solutions">
-  <App />
-</BrowserRouter>;
